ff843421f911790bb9b1dc77a18b0449179360a2
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 23:38:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  new_sentence_array = []
-  sentence_array = sentence.split
-    sentence_array.each do |word|
-    new_sentence_array << word.reverse
-  end
-  return new_sentence_array.join(" ")
-end
+# def reverse_each_word(sentence)
+#   new_sentence_array = []
+#   sentence_array = sentence.split
+#     sentence_array.each do |word|
+#     new_sentence_array << word.reverse
+#   end
+#   return new_sentence_array.join(" ")
+# end
 
 def reverse_each_word(sentence)
   new_array = []
@@ -13,5 +13,5 @@ def reverse_each_word(sentence)
     array.collect do |word|
       array << word.reverse
     end
-    new_array.join
+    new_array.join(" ")
 end
